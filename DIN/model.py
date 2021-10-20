ffa87ce1739ce57238bb8aaa13c71ba5eb1d09e9
吕鑫 <562331245@qq.com>
2021-10-20 22:48:01 +0800

    embedding

--- DIN/model.py
+++ DIN/model.py
@@ -1,5 +1,7 @@
+from keras.regularizers import l2
 from tensorflow.keras import Model
 import tensorflow as tf
+from tensorflow.keras.layers import Embedding
 
 
 class DIN(Model):
@@ -29,9 +31,35 @@ class DIN(Model):
         self.dense_len = len(self.dense_feature_columns)
         self.behavior_len = len(behavior_feature_list)  # 1
 
+        # history seq 嵌入层
+        self.seq_embedding_layer = [Embedding(input_dim=feat['feat_num'],
+                                              output_dim=feat['embed_dim'],
+                                              embeddings_initializer='random_uniform',
+                                              embeddings_regularizer=l2(embed_reg),
+                                              input_length=1)
+                                    for feat in self.sparse_feature_columns
+                                    if feat not in behavior_feature_list
+                                    ]
+        # user behavior嵌入层
+        self.behavior_embedding_layer = [Embedding(input_dim=feat['feat_num'],
+                                                   output_dim=feat['embed_dim'],
+                                                   embeddings_initializer='random_uniform',
+                                                   embeddings_regularizer=l2(embed_reg),
+                                                   input_length=1)
+                                         for feat in self.sparse_feature_columns
+                                         if feat in behavior_feature_list]
+
     def call(self, inputs, training=None, mask=None):
+        # dense_inputs :连续特征， sparse_inputs: 离散特征， seq_inputs:user behavior， item_inputs候补特征
         dense_inputs, sparse_inputs, seq_inputs, item_inputs = inputs
 
         # attention ---> mask, if the element of seq_inputs is equal 0, it must be filled in.  ???
         mask = tf.cast(tf.not_equal(seq_inputs[:, :, 0], 0),
                        dtype=tf.float32)  # (None, maxlen), cast()数据类型转换。bool转换为float类型，False->0.,True->1.
+
+        # history seq embedding
+        seq_embed = tf.concat([self.seq_embedding_layer[i](seq_inputs[0, 0, i])
+                               for i in range(self.other_sparse_len)], axis=-1)
+        # user behavior embedding
+        behavior_embedded = tf.concat([self.behavior_embedding_layer[i](item_inputs[0, 0, i])
+                                       for i in range(self.behavior_len)], axis=-1)
