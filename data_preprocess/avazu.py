5c38d049b495952ae4f7105e72d4d4105552db6e
lvxin <562331245@qq.com>
2022-04-23 19:18:46 +0800

    avazu data preprocess

--- data_preprocess/avazu.py
+++ data_preprocess/avazu.py
@@ -3,10 +3,38 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+import joblib
 from data_preprocess.utils import sparse_faeture_dict
+
+# todo 有的好的硬件再测试吧，test代码在kaggle上
+"""
+11天的数据 : train.csv 4kw+数据， test.csv： 400w+数据
+avazu dataset field:
+id : 广告id , id 算是离散特征
+click： 0/1
+hour: format is YYMMDDHH, so 14091123 means 23:00 on Sept. 11, 2014 UTC.
+banner_pos
+site_id
+site_domain
+site_category
+app_id
+app_domain
+app_category
+device_id
+device_ip
+device_model
+device_type
+device_conn_type
+C14-C21 -- anonymized categorical variables
+"""
 
 
 def create_avazu_dataset(path, read_part=True, samples_num=5000, embed_dim=8):
+    sparse_features = ['day', 'hour', 'id', 'C1', 'banner_pos', 'site_id', 'site_domain',
+                       'site_category', 'app_id', 'app_domain', 'app_category', 'device_id',
+                       'device_ip', 'device_model', 'device_type', 'device_conn_type', 'C14',
+                       'C15', 'C16', 'C17', 'C18', 'C19', 'C20', 'C21']
+
     train_path = path + '/train.csv'
     print('加载数据集')
     if read_part:
@@ -14,15 +42,17 @@ def create_avazu_dataset(path, read_part=True, samples_num=5000, embed_dim=8):
     else:
         train_data = pd.read_csv(path)
 
-    # 暂时舍弃第C20， 数据上下差别太大应该预处理
-    train_data[['C20', 'C21']] = train_data[['C21', 'C20']]
-    sparse_features = train_data.columns.tolist()[3:-1]
-    need_transform_feat = train_data.columns.tolist()[5:-10]
-
-    # 也可以 Hash Encoder
+    # hour, 只有14年10月11天的数据，year,month没必要做特征
+    train_data['hour'] = train_data['hour'].apply(str)
+    train_data['day'], train_data['hour'] = train_data['hour'].map(lambda x: int(x[4:6])), \
+                                            train_data['hour'].map(lambda x: int(x[6:8]))  # int强转去掉字符串前的0
+    # sparse features
     le = LabelEncoder()
-    for feat in need_transform_feat:
+    for feat in sparse_features:
         train_data[feat] = le.fit_transform(train_data[feat])
+
+    # save LabelEncoder model for test
+    # joblib.dump(le, 'label_encoder.model')
 
     features_columns = [sparse_faeture_dict(feat_name=feat, feat_num=train_data[feat].max() + 1, embed_dim=embed_dim)
                         for feat in sparse_features]
@@ -41,7 +71,8 @@ def avazu_dataset_analysis(path, read_part=True, samples_num=5000):
     """
     https://blog.csdn.net/u014128608/article/details/93393175
     """
-    train_data = pd.read_csv(path, nrows=samples_num)
+    train_path = path + '/train.csv'
+    train_data = pd.read_csv(train_path, nrows=samples_num)
     # dataset summary
     # print(train_data.info(verbose=True))
 
@@ -49,16 +80,27 @@ def avazu_dataset_analysis(path, read_part=True, samples_num=5000):
     # print(train_data.isnull().any())
 
     # 数据分布
-    print(train_data.describe())
-    for i in range(14, 22):
-        sns.countplot(train_data['C' + str(i)])  # 更换filed，查看不同filed的分布
-        plt.show()
+    # print(train_data.describe())
+    # for i in range(14, 22):
+    #     sns.countplot(train_data['C' + str(i)])  # 更换filed，查看不同filed的分布
+    #     plt.show()
+
+    print(train_data['C1'].unique())
+
+
+def gen_commit_result(path):
+    res = pd.read_csv(path + '/res.csv')
+    result = pd.read_csv(path + '/result.csv')
+    res['click'] = result['click']
+    res.to_csv('res.csv', index=False)
 
 
 # if __name__ == '__main__':
-#     root_path = 'D:/data/avazu-ctr-prediction/train.csv'
+    # root_path = 'D:/data/avazu-ctr-prediction'
 
     # dataset analysis
     # avazu_dataset_analysis(root_path)
 
     # create_avazu_dataset(root_path)
+
+    # gen_commit_result(root_path)
