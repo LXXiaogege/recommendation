c3bb360088c90977868904938442c9fe87d820d9
lvxin <562331245@qq.com>
2022-04-25 16:46:23 +0800

    avazu data preprocess

--- CCPM/train.py
+++ CCPM/train.py
@@ -1,25 +1,30 @@
+import pandas as pd
+
 from data_preprocess.avazu import create_avazu_dataset
 from model import CCPM
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import binary_crossentropy
+from tensorflow.keras.callbacks import EarlyStopping
 
-path = 'D:/data/avazu-ctr-prediction'
+path = '../data/avazu-ctr-prediction'
 
 embed_dim = 11
 conv_kernel_width = (6, 5, 3)
 conv_filters = (4, 4, 4)
 
 learning_rate = 0.001
-batch_size = 32
-epochs = 10
+batch_size = 64
+epochs = 20
 
 # samples_num 尽量大，避免过拟合
-(train_x, train_y), (val_x, val_y), features_columns = create_avazu_dataset(path, read_part=True, samples_num=100000,
-                                                                            embed_dim=embed_dim)
+(train_x, train_y), (val_x, val_y), test_x, features_columns = create_avazu_dataset(path, read_part=True,
+                                                                                    samples_num=1000000,
+                                                                                    embed_dim=embed_dim)
 
 model = CCPM(feat_column=features_columns, conv_kernel_width=conv_kernel_width, conv_filters=conv_filters)
 model.compile(optimizer=Adam(), loss=binary_crossentropy)
-model.fit(x=train_x, y=train_y, batch_size=batch_size, epochs=epochs, validation_split=0.1, shuffle=True)
+model.fit(x=train_x, y=train_y, batch_size=batch_size, epochs=epochs, validation_split=0.1, shuffle=True,
+          callbacks=[EarlyStopping(monitor='val_loss', patience=2, restore_best_weights=True)])
 
-# predict failed ：avazu 数据集太大无法完全加载，导致test data 里的某些特征超过了用于训练的数据集中的embedding vocabulary
-# model.predict()
+result = model.predict(test_x, batch_size=10000)
+pd.Series(result.flatten()).to_csv('result.csv', header=None, index=False)
--- data_preprocess/avazu.py
+++ data_preprocess/avazu.py
@@ -30,30 +30,41 @@ C14-C21 -- anonymized categorical variables
 
 
 def create_avazu_dataset(path, read_part=True, samples_num=5000, embed_dim=8):
-    sparse_features = ['day', 'hour', 'id', 'C1', 'banner_pos', 'site_id', 'site_domain',
+    print('数据预处理开始')
+    sparse_features = ['hour', 'id', 'C1', 'banner_pos', 'site_id', 'site_domain',
                        'site_category', 'app_id', 'app_domain', 'app_category', 'device_id',
                        'device_ip', 'device_model', 'device_type', 'device_conn_type', 'C14',
                        'C15', 'C16', 'C17', 'C18', 'C19', 'C20', 'C21']
 
-    train_path = path + '/train.csv'
+    train_path = path + '/train.gz'
+    test_path = path + '/test.gz'
     print('加载数据集')
     if read_part:
         train_data = pd.read_csv(train_path, nrows=samples_num)
     else:
-        train_data = pd.read_csv(path)
+        train_data = pd.read_csv(train_path)
+    test_x = pd.read_csv(test_path)
 
     # hour, 只有14年10月11天的数据，year,month没必要做特征
     train_data['hour'] = train_data['hour'].apply(str)
-    train_data['day'], train_data['hour'] = train_data['hour'].map(lambda x: int(x[4:6])), \
-                                            train_data['hour'].map(lambda x: int(x[6:8]))  # int强转去掉字符串前的0
+    train_data['hour'] = train_data['hour'].map(lambda x: int(x[6:8]))  # int强转去掉字符串前的0
+    test_x['hour'] = test_x['hour'].apply(str)
+    test_x['hour'] = test_x['hour'].map(lambda x: int(x[6:8]))
+    print('加载数据完成')
+    print('Sparse feature encode')
     # sparse features
     le = LabelEncoder()
     for feat in sparse_features:
-        train_data[feat] = le.fit_transform(train_data[feat])
+        all_class = pd.concat([train_data[feat], test_x[feat]]).unique()
+        le.fit(all_class)
+        train_data[feat] = le.transform(train_data[feat])
+        test_x[feat] = le.transform(test_x[feat])
 
+    print('Sparse feature encode succeed')
     # save LabelEncoder model for test
     # joblib.dump(le, 'label_encoder.model')
-
+    # sparse_faeture_dict(feat_name='day', feat_num=32, embed_dim=embed_dim)
+    # sparse_faeture_dict(feat_name='hour', feat_num=24, embed_dim=embed_dim)
     features_columns = [sparse_faeture_dict(feat_name=feat, feat_num=train_data[feat].max() + 1, embed_dim=embed_dim)
                         for feat in sparse_features]
 
@@ -62,9 +73,10 @@ def create_avazu_dataset(path, read_part=True, samples_num=5000, embed_dim=8):
     train_y = train['click'].values.astype('int32')
     val_x = val[sparse_features].values.astype('int32')
     val_y = val['click'].values.astype('int32')
+    test_x = test_x[sparse_features].values.astype('int32')
 
-    print('加载数据完成')
-    return (train_x, train_y), (val_x, val_y), features_columns
+    print('数据预处理完成')
+    return (train_x, train_y), (val_x, val_y), test_x, features_columns
 
 
 def avazu_dataset_analysis(path, read_part=True, samples_num=5000):
@@ -75,17 +87,14 @@ def avazu_dataset_analysis(path, read_part=True, samples_num=5000):
     train_data = pd.read_csv(train_path, nrows=samples_num)
     # dataset summary
     # print(train_data.info(verbose=True))
-
     # 查看缺失值
     # print(train_data.isnull().any())
-
     # 数据分布
     # print(train_data.describe())
     # for i in range(14, 22):
     #     sns.countplot(train_data['C' + str(i)])  # 更换filed，查看不同filed的分布
     #     plt.show()
-
-    print(train_data['C1'].unique())
+    # print(train_data['C1'].unique())
 
 
 def gen_commit_result(path):
@@ -96,8 +105,7 @@ def gen_commit_result(path):
 
 
 # if __name__ == '__main__':
-    # root_path = 'D:/data/avazu-ctr-prediction'
-
+#     root_path = 'D:/data/avazu-ctr-prediction'
     # dataset analysis
     # avazu_dataset_analysis(root_path)
 
